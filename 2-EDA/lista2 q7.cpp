85a1b7cbbce070b82d58f27cbeeaee072298aa43
Patrick Cruz <145387737+cruzPatrick@users.noreply.github.com>
2025-05-06 11:41:48 -0300

    more exercises
    and codes from the OS book

--- 2-EDA/lista2 q7.cpp
+++ 2-EDA/lista2 q7.cpp
@@ -1,8 +1,17 @@
 /*
 7.Construa uma função que insira sempre em ordem crescente.
-    fazer um algoritmo para achar onde esse valor tem que entrar
+    Minhas ideias pra resolver:
+    fazer um algoritmo para achar onde esse valor tem que entrar (usar menor/maior?)
     Se lista for NULL esse vai ser o maior valor
+    se tiver um, tenho que descobrir se fica a esquerda ou se fica a direita
+    se tiver mais de um eu choro em posição fetal pq a lista já tem que estar ordenada
 */
 #include <iostream>
 using namespace std;
 
+struct no{
+    int info;
+    struct no* prox;
+};
+typedef struct no* noPtr;
+noPtr topo = NULL;
